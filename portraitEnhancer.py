95b3a97a819b609453907a2bc96a126d2a0f83e0
Jonathan Tan <e0032273@u.nus.edu>
2017-11-11 03:19:21 +0800

    Improved teeth detection and whitening. Check readme for updates to dependencies

--- portraitEnhancer.py
+++ portraitEnhancer.py
@@ -1,113 +1,59 @@
 import cv2	
 import sys
-import skimage
+import imutils
+import dlib
 import numpy as np
 from filters import face_filter, whiten_teeth
+from imutils import face_utils
 
 def main (argV = None):
 	# Get user supplied values
 	imagePath = sys.argv[1]
-	faceFilterFactor = sys.argv[2];
-	whiteningFactor = sys.argv[3];
+	faceFilterFactor = sys.argv[2]
+	whiteningFactor = sys.argv[3]
 	whiteningFactor = float(whiteningFactor)/100
 	faceFilterFactor = float(faceFilterFactor)/100
-	faceCascPath = "haarcascade_frontalface_default.xml"
-	smileCascPath = "haarcascade_smile.xml"
-
-	# Create the haar cascade
-	faceCascade = cv2.CascadeClassifier(faceCascPath)
-	smileCascade = cv2.CascadeClassifier(smileCascPath)
 
 	# Read the image
 	image = cv2.imread(imagePath)
 	# Convert to grayscale
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-	smile_x = 0
-	smile_y = 0
-	smile_w = 0
-	smile_h = 0	
+	detector = dlib.get_frontal_face_detector()
+	predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+	rects = detector(gray,1)
+	overlay = np.copy(image)
+	overlay[:] = [0]
 
-	face_x = 0
-	face_y = 0
-	face_w = 0
-	face_h = 0
+	originalImage = np.copy(image)
 
-	# Detect face in the image
-	sF = 1.01
-	while 1:
-		faces = faceCascade.detectMultiScale(
-		    gray,
-		    scaleFactor=sF,
-		    minNeighbors=7,
-		    minSize=(55, 55)
-		)
-		if len(faces) == 1:
-			break
-		if sF > 1.3:
-			print("Sorry! A face could not be detected Try another image")
-			sys.exit()
-		sF = sF + 0.01
-	#sF = 1.01
-	for (x,y,w,h) in faces:
-		face_x = x
-		face_y = y
-		face_w = w
-		face_h = h
-    
-    # Detect smiles in the image
-	sF = 1.2
-	while 1:
-		smiles = smileCascade.detectMultiScale(
-		    gray,
-		    scaleFactor=sF,
-		    minNeighbors=8,
-		    minSize=(15, 15)
-		)
-		if len(smiles) == 1 or sF > 1.8:
-			break
-		sF = sF + 0.1
+	for (i, rect) in enumerate(rects):
+		shape = predictor(gray, rect)
+		shape = face_utils.shape_to_np(shape)
+		for (name, (i,j)) in face_utils.FACIAL_LANDMARKS_IDXS.items():
+			if name == "mouth":
+				(x,y) = face_utils.FACIAL_LANDMARKS_IDXS[name]
+				pts = shape [x:y]
+				hull = cv2.convexHull(pts)
+				cv2.drawContours(overlay, [hull], -1, (255,255,255), -1)
+			
+	overlay = cv2.bitwise_and(overlay, image)
+
+	whiten_teeth_image = whiten_teeth(overlay, whiteningFactor)
 	
-	maxWhite = 0
-	# Iterates through all smiles found and finds the one most likely to be a smile, based on percentage of white 
-	for (x,y,w,h) in smiles:
-		# Gets cropped image
-		croppedImage = image[y:(y+h), x:(x+w)]
-		# Converts to grayscale
-		croppedImage = cv2.cvtColor(croppedImage, cv2.COLOR_BGR2GRAY)
-		# Get B&W histogram
-		hist = cv2.calcHist([croppedImage],[0],None,[256],[0,256])
-		# Calculates percentage of white
-		whiteSum = 0;
-		for i in range(199,255):
-			whiteSum = whiteSum + hist[i]
-		percentageWhite = whiteSum / (h*w)
-		# Maintains max
-		if percentageWhite > maxWhite:
-			maxWhite = whiteSum
-			smile_x = x
-			smile_y = y
-			smile_w = w
-			smile_h = h
-	
-	onlyFaceFilter = np.copy(image)
-	#cv2.imshow("Before", image)
-	whiten_teeth_image = whiten_teeth(image[smile_y:(smile_y+smile_h), smile_x:(smile_x+smile_w)], whiteningFactor)
-	
-	image[smile_y:(smile_y+smile_h), smile_x:(smile_x+smile_w)] = cv2.addWeighted(image[smile_y:(smile_y+smile_h), smile_x:(smile_x+smile_w)], 0.5, whiten_teeth_image, 0.5, 0)
-	#cv2.imshow("Only Teeth Whitening", image)
+	image [:,:,0] = (image [:,:,0] * 0.5) + (whiten_teeth_image[:,:,0]* 0.5 *whiten_teeth_image[:,:,3]) + (image [:,:,0] * 0.5) - (image [:,:,0] * 0.5 * whiten_teeth_image[:,:,3])
+	image [:,:,1] = (image [:,:,1] * 0.5) + (whiten_teeth_image[:,:,1]* 0.5 *whiten_teeth_image[:,:,3]) + (image [:,:,1] * 0.5) - (image [:,:,1] * 0.5 * whiten_teeth_image[:,:,3])
+	image [:,:,2] = (image [:,:,2] * 0.5) + (whiten_teeth_image[:,:,2]* 0.5 *whiten_teeth_image[:,:,3]) + (image [:,:,2] * 0.5) - (image [:,:,2] * 0.5 * whiten_teeth_image[:,:,3])
+	#cv2.imshow("Only Teeth Whitening",image)
 	cv2.imwrite('./outImages/teeth_whitening_only.jpg', image)
-	
-	onlyFaceFilter = face_filter(onlyFaceFilter)
-	#cv2.imshow("Only Face Filter", onlyFaceFilter)
+
+	onlyFaceFilter = face_filter(originalImage)
+	onlyFaceFilter = cv2.addWeighted(originalImage, (1-faceFilterFactor), onlyFaceFilter, faceFilterFactor, 0)
 	cv2.imwrite('./outImages/face_filter_only.jpg', onlyFaceFilter)
 
 	image = cv2.addWeighted(image, (1-faceFilterFactor), onlyFaceFilter, faceFilterFactor, 0)
 	#cv2.imshow("Teeth Whitening + Face Filter", image)
 	cv2.imwrite('./outImages/teeth_whitening_and_face_filter.jpg', image)
-
-	cv2.waitKey(0)
-
 
 
 
